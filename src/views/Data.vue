23c2ff5bec2143d8bf1f48c0d1b2351e6e2c1be6
Julius Kirkegaard <juliusbierk@gmail.com>
2021-01-20 11:21:13 +0100

    Example data

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -21,7 +21,8 @@
             </div>
           </div>
           <div class="window-content p-2">
-            <div v-show="!file_chosen" class="row flex-justify-center">
+
+            <div class="row flex-justify-center">
               <div class="cell-6">
                 <input
                   id="file"
@@ -33,14 +34,57 @@
               </div>
             </div>
 
-            <div class="row">
+            <div v-if="!filename" class="row flex-justify-center">
+              <div class="cell-5">
+                Files must be in a .csv or .tsv format
+                <button v-if="!show_example" @click="show_example = true;" style="margin-bottom:2px" class="defaultcursor button mini rounded">Show Example</button>
+              </div>
+            </div>
+
+            <div v-if="filename" class="row">
+              <div class="cell-6 offset-3">
+                <label class="switch transition-on">
+                  <input
+                    type="checkbox"
+                    data-role="switch"
+                    v-model="has_header"
+                    data-role-switch="true"
+                    class=""
+                  /><span class="check"></span>
+                  <span class="caption">{{
+                    has_header ? "First row is header" : "No header"
+                  }}</span>
+                </label>
+
+                <label class="switch transition-on">
+                  <input
+                    type="checkbox"
+                    data-role="switch"
+                    v-model="multiple_x_axes"
+                    data-role-switch="true"
+                    class=""
+                  /><span class="check"></span>
+                  <span class="caption">{{
+                    multiple_x_axes
+                      ? "Every second column are x-axes"
+                      : "First column is x-axis"
+                  }}</span>
+                </label>
+              </div>
+            </div>
+
+            <div v-if="filename || show_example" class="row">
               <div class="cell-8 offset-2">
-                <b>{{ filename }}:</b>
+                <b v-show="filename">{{ filename }}:</b>
                 <table class="table">
                   <thead>
                     <tr>
                       <th
-                        v-bind:class="{ 'bg-lightTime': i === 1 }"
+                        v-bind:class="{
+                          'bg-lightTime': multiple_x_axes
+                            ? i % 2 === 1
+                            : i === 1
+                        }"
                         v-for="i in header.length"
                         v-bind:key="i"
                         v-html="header[i - 1]"
@@ -50,7 +94,9 @@
                   <tbody>
                     <tr v-for="i in data.length" v-bind:key="i">
                       <td
-                        v-bind:class="{ 'bg-time': j === 1 }"
+                        v-bind:class="{
+                          'bg-time': multiple_x_axes ? j % 2 === 1 : j === 1
+                        }"
                         v-for="j in data[i - 1].length"
                         v-bind:key="j"
                         v-html="data[i - 1][j - 1]"
@@ -74,22 +120,22 @@ export default {
     return {
       py: "http://127.0.0.1:7555",
       create_open: true,
-      file_chosen: false,
       header: ["x", "y_1", "y_2", "y_3"],
       data: [
         ["0", "1.3", "1.1", "1.5"],
         ["1", "2.1", "2.5", "2.3"],
         ["2", "3.3", "3.6", "3.1"]
       ],
-      filename: "Example data"
+      filename: null,
+      multiple_x_axes: false,
+        has_header: true,
+        show_example: false,
     };
   },
   methods: {
     upload(e) {
       var files = e.target.files;
       if (files) {
-        // this.file_chosen = true;
-
         // For now just do one file:
         const formData = new FormData();
         for (const file of files) {
