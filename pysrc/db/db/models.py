09da2970f24bc63e59c614738e0b83c1057304aa
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2021-02-19 11:04:40 +0100

    django db

--- pysrc/db/db/models.py
+++ pysrc/db/db/models.py
@@ -6,5 +6,15 @@ class Dataset(models.Model):
     name = models.CharField(max_length=500)
     parent = models.CharField(max_length=500)
     info = models.TextField()
-    content = models.TextField()
+    content = models.TextField()  # JSON
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=500, primary_key=True)
+    content = models.TextField()  # JSON
+
+
+class Fit(models.Model):
+    name = models.CharField(max_length=500, primary_key=True)
+    content = models.TextField()  # JSON
 
